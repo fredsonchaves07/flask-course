f14ee8f18281db6857af1aa8f283245c3e888162
Fredson Chaves <fredsonchaves07@gmail.com>
2020-11-18 11:42:35 -0300

    Estrutura inicial do SQLAlchemy

---
+++ 5a/app.py
@@ -0,0 +1,60 @@
+from flask import Flask, render_template, session, redirect, url_for, flash
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+import os
+from flask_sqlalchemy import SQLAlchemy
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'Desenvolvedor Python'
+app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{os.path.join(basedir, 'data.sqlite')}'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+bootstrap = Bootstrap(app)
+moment = Moment(app)
+db = SQLAlchemy(app)
+
+class NameForm(FlaskForm):
+    name = StringField('Informe o seu nome: ', validators=[DataRequired()])
+    submit = SubmitField('Enviar')
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    name = None
+    form = NameForm()
+
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Você deve alterar o seu nome!')
+        session['name'] = form.name.data
+        form.name.data = ''
+
+        return redirect(url_for('index'))
+        
+    return render_template('index.html',
+                            current_time=datetime.utcnow(),
+                            form=form,
+                            name=session.get('name'))
+
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html', name=name)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_error_serve(e):
+    return render_template('500.html'), 500
