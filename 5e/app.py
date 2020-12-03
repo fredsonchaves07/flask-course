887b8ee494e4249744c9fbe28918eb7e38dcd1b8
Fredson Chaves <fredsonchaves07@gmail.com>
2020-12-03 14:56:33 -0300

    Entendendo as operações do flask migrate

---
+++ 5e/app.py
@@ -0,0 +1,86 @@
+from flask import Flask, render_template, session, redirect, url_for, flash
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+import os
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'Desenvolvedor Python'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+bootstrap = Bootstrap(app)
+moment = Moment(app)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+# Models
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+
+    users = db.relationship('User', backref='role')
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+
+class NameForm(FlaskForm):
+    name = StringField('Informe o seu nome: ', validators=[DataRequired()])
+    submit = SubmitField('Enviar')
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    form = NameForm()
+
+    if form.validate_on_submit():
+        user = User.query.filter_by(name=form.name.data).first()
+
+        if user is None:
+            user = User(name=form.name.data)
+            db.session.add(user)
+            db.session.commit()
+            session['known'] = False
+        else:
+            session['known'] = True    
+            session['name'] = form.name.data
+            form.name.data = ''
+            return redirect(url_for('index'))
+
+        return render_template('index.html', form=form, name=session.get('name'), known=session.get('known', False))
+
+        
+    return render_template('index.html',
+                            current_time=datetime.utcnow(),
+                            form=form,
+                            name=session.get('name'))
+
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html', name=name)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_error_serve(e):
+    return render_template('500.html'), 500
