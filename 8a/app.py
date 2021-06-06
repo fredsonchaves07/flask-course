0521f4b8af451e7f9d220f7504ce620f47f04d46
Fredson Chaves <fredsonchaves07@gmail.com>
2021-06-06 12:14:11 -0300

    Corrigindo problemas de tests

--- 8a/app.py
+++ 8a/app.py
@@ -11,12 +11,8 @@ def make_shell_context():
 
 
 @app.cli.command()
-@click.argument('test_names', nargs=-1)
-def test(test_names):
+def test():
     """Run the unit tests."""
     import unittest
-    if test_names:
-        tests = unittest.TestLoader().loadTestsFromNames(test_names)
-    else:
-        tests = unittest.TestLoader().discover('tests')
+    tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
