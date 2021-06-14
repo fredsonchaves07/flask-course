f41b77c353508793915ae8a8d5f25343a19876f4
Fredson Chaves <fredsonchaves07@gmail.com>
2021-06-14 09:20:06 -0300

    Editando perfis comuns e administradores

--- 10b/app/main/views.py
+++ 10b/app/main/views.py
@@ -3,9 +3,9 @@ from flask import render_template, session, redirect, url_for, flash
 from flask_login import login_required, current_user
 from ..decorators import admin_required, permission_required
 from . import main
-from .forms import EditProfileForm, NameForm
+from .forms import EditProfileAdminForm, EditProfileForm, NameForm
 from .. import db
-from ..models import Permision, User 
+from ..models import Permision, User, Role
 
 
 @main.route('/admin')
@@ -82,3 +82,30 @@ def edit_profile():
     form.about_me.data = current_user.about_me
     
     return render_template('edit_profile.html', form=form)
+
+@main.route('/edit-profile/<int:id>', methods=['GET', 'POST'])
+@login_required
+@admin_required
+def edit_profile_admin(id):
+    user = User.query.get_or_404(id)
+    form = EditProfileAdminForm(user=user)
+    if form.validate_on_submit():
+        user.email = form.email.data
+        user.username = form.username.data
+        user.confirmed = form.confirmed.data
+        user.role = Role.query.get(form.role.data)
+        user.name = form.name.data
+        user.location = form.location.data
+        user.about_me = form.about_me.data
+        db.session.add(user)
+        db.session.commit()
+        flash('The profile has ben updated.')
+        return redirect(url_for('.user', username=user.username))
+    form.email.data = user.email
+    form.username.data = user.username
+    form.confirmed.data = user.confirmed
+    Role.query.get(form.role.data) = user.role
+    form.name.data = user.name
+    form.location.data = user.location
+    form.about_me.data = user.about_me
+    return render_template('edit_profile.html', form=form, user=user)
