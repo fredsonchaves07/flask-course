c1262ef67dc3a8f33b1421f272af8ae6895fd211
Fredson Chaves <fredsonchaves07@gmail.com>
2021-06-22 14:32:44 -0300

    listagem de postagem dos usuarios

---
+++ 11b/app/models.py
@@ -0,0 +1,169 @@
+import hashlib
+
+from datetime import datetime
+from flask_migrate import current
+from sqlalchemy.orm import backref
+from . import db
+from . import login_manager
+from flask import current_app, request
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin, AnonymousUserMixin
+
+
+class Permision:
+    FOLLOW = 1
+    COMMENT = 2
+    WRITE = 4
+    MODERATE = 8
+    ADMIN = 16
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    default = db.Column(db.Boolean, default=False, index=True)
+    permissions = db.Column(db.Integer)
+    users = db.relationship('User', backref='role', lazy='dynamic')
+    
+    def __init__(self, **kwargs):
+        super(Role, self).__init__(**kwargs)
+        if self.permissions is None:
+            self.permissions = 0
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+    
+    def add_permission(self, perm):
+        if not self.has_permission(perm):
+            self.permissions += perm
+    
+    def remove_permission(self, perm):
+        if not self.has_permission(perm):
+            self.permissions -= perm
+            
+    def reset_permission(self):
+        self.permissions = 0
+    
+    def has_permission(self, perm):
+        return self.permissions & perm == perm
+    
+    @staticmethod
+    def insert_role():
+        roles = {
+            'User': [Permision.FOLLOW, Permision.COMMENT, Permision.WRITE],
+            'Moderator': [Permision.FOLLOW, Permision.COMMENT, Permision.WRITE, Permision.MODERATE],
+            'Administrator': [Permision.FOLLOW, Permision.COMMENT, Permision.WRITE, Permision.MODERATE, Permision.ADMIN]
+        }
+        
+        default_role = 'User'
+        for r in roles:
+            role = Role.query.filter_by(name=r).first()
+            
+            if role is None:
+                role = Role(name=r)
+            role.reset_permission()
+            
+            for perm in roles[r]:
+                role.add_permission(perm)
+            role.default = (role.name == default_role)
+            db.session.add(role)
+        db. session.commit()
+
+
+class User(UserMixin, db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(64), unique=True, index=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    password_hash = db.Column(db.String(128))
+    confirmed = db.Column(db.Boolean, default=False)
+    name = db.Column(db.String(64))
+    location = db.Column(db.String(64))
+    about_me = db.Column(db.Text())
+    member_since = db.Column(db.DateTime(), default=datetime.utcnow)
+    last_seen = db.Column(db.DateTime(), default=datetime.utcnow)
+    post = db.relationship('Post', backref='author', lazy='dynamic')
+    
+    def __init__(self, **kwargs):
+        super(User, self).__init__(**kwargs)
+        if self.role is None:
+            if self.email == current_app.config['FLASK_ADMIN']:
+                self.role = Role.query.filter_by(name='Administrator').first()
+            if self.role is None:
+                self.role = Role.query.filter_by(default=True).first()
+    
+    @property
+    def password(self):
+        raise AttributeError('Password is not a readable attribute')
+    
+    
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+        
+    
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
+    
+    def generate_confirm_token(self, expiration=3600):
+        s = Serializer(current_app.config['SECRET_KEY'], expiration)
+        
+        return s.dumps({'confirm': self.id}).decode('utf-8')
+    
+    def confirm(self, token):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token.encode('utf-8'))
+        except:
+            return False
+        if data.get('confirm') != self.id:
+            return False
+        self.confirmed = True
+        db.session.add(self)
+        return True
+    
+    def can(self, perm):
+        return self.role is not None and self.role.has_permission(perm)
+
+    def is_administrator(self):
+        return self.can(Permision.ADMIN)
+    
+    def ping(self):
+        self.last_seen = datetime.utcnow()
+        db.session.add(self)
+        db.session.commit()
+        
+    def generate_confirmation_token(self, expiration=3600):
+        s = Serializer(current_app.config['SECRET_KEY'], expiration)
+        return s.dumps({'confirm': self.id}).decode('utf-8')
+    
+    def gravatar(self, size=100, default='identicon', rating=9):
+        url = 'https://secure.gravatar.com/avatar'
+        hash = hashlib.md5(self.email.lower().encode('utf-8')).hexdigest()
+        return '{url}/{hash}?s={size}&d={default}&r={rating}'.format(url=url, hash=hash, size=size, default=default, rating=rating)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+    
+class AnonymousUser(AnonymousUserMixin):
+    def can(self, permissions):
+        return False
+
+    def is_administrator(self):
+        return False
+    
+login_manager.anonymous_user = AnonymousUser
+    
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class Post(db.Model):
+    __tablename__ = 'posts'
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.Text)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.now)
+    author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    
---
+++ 11b/config.py
@@ -0,0 +1,41 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'Desenvolvedor Python'
+    MAIL_SERVER = os.environ.get('MAIL_SERVER') or 'smtp.mailtrap.io'
+    MAIL_PORT = os.environ.get('MAIL_PORT') or 2525
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') or True
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME') or 'c0ce86e978a7d3'
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD') or '39feb26c850205'
+    FLASK_MAIL_SUBJECT_PREFIX = '[Flasky]'
+    FLASK_MAIL_SENDER = 'Flask Admin <flasky@example.com>'
+    FLASK_ADMIN = os.environ.get('FLASK_ADMIN')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
+
+class TestConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or 'sqlite:///'
+
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+
+config = {
+    'development': DevelopmentConfig,
+    'testing': TestConfig,
+    'production': ProductionConfig,
+
+    'default': DevelopmentConfig
+}
+
+
+
